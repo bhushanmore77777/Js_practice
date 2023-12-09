507d943b38d290c373d654d326cc79b5e6e4be0c
bhushanmore77777 <131691203+bhushanmore77777@users.noreply.github.com>
2023-12-09 17:37:10 +0000

    DataTypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=144553
+let accountEmail="bhushan@gmail.com"
+var accountPassword="123456"
+accountCity="Nashik"
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+/*
+DataTypes and ECMA Standards
+"use strict";---->treat all js code as newer version
+alert(3+3)---->this will not run because this is nodejs not browser
+ECMA---->It involves all language specification standards
+
+****DataTypes****
+1.number---->2^53
+2.string
+3.bigint---->for big values like stocks
+4.boolean---->true/false
+5.null---->standalone value
+6.undefined---->value is declared but not initialized
+7.symbol---->unique
+
+Object
+*/
+console.log(typeof null)//---->object
+console.log(typeof undefined)//---->undefined
+
+
