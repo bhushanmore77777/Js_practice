4894339b22bb63feef41fe7d191a0004f2f94475
bhushanmore77777 <131691203+bhushanmore77777@users.noreply.github.com>
2023-12-10 07:18:10 +0000

    Conversion_of_DataTypes

---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score=33
+console.log(typeof score)//number
+let  score1="33"
+console.log(typeof score1)//string
+let score2="33abcd"
+console.log(typeof score2)//string
+console.log(score2)
+
+//33---->33
+//"33abcd"---->NaN
+//true---->1
+//false---->0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)//true
+console.log(typeof booleanIsLoggedIn)//boolean
+let isLoggedIn1=""
+let booleanIsLoggedIn1=Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1)//false
+let isLoggedIn2="BHUSHAN"
+let booleanIsLoggedIn2=Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2)//true
+//""---->false
+//"BHUSHAN"---->true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)//33
+console.log(typeof stringNumber)//string
+
