ba65760cddcab99ccff4bc8be689b984bc30b5b6
bhushanmore77777 <131691203+bhushanmore77777@users.noreply.github.com>
2023-12-19 08:13:56 +0000

    while do while loop

---
+++ .devcontainer/05_Iteration/02_while_do_while_loop.js
@@ -0,0 +1,19 @@
+let i=0
+while(i<=10)
+{
+    console.log(`the value of i is ${i}`)
+    i++
+}
+let myArray=["Flash","Batman","Superman"]
+let arr=0
+while(arr<myArray.length)
+{
+    console.log(`the value is ${myArray[arr]}`)
+    arr++
+}
+let score=1
+do
+{
+    console.log(`score is ${score}`)
+    score++
+}while(score<=10)
